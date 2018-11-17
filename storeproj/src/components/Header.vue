1f8b23e393864805f0d4b14e561cca9b6aa97555
WillKirkhope <kirkhopetraining@yahoo.com>
2018-11-17 11:24:14 -0700

    Added components and started styling

---
+++ storeproj/src/components/Header.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <div class="nav">
+    <h1>Logo</h1>
+    <div class="icons">
+    <img id="search" src="../assets/search.png" alt="">
+    <img id="cart" src="../assets/cart.svg.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+
+.nav{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: solid black 2px;
+}
+
+.icons{
+  margin-right: 5vw;
+}
+
+#search{
+  margin-right: 1vw;
+}
+
+h1{
+  margin-left: 5vw;
+}
+
+img{
+  width: 2vw;
+}
+
+</style>
---
+++ storeproj/src/components/Landing.vue
@@ -0,0 +1,58 @@
+<template lang="html">
+  <div class="landing">
+    <div class="side-nav">
+      <a href="#">Shirts</a>
+      <a href="#">Hats</a>
+      <a href="#">Hoddies</a>
+      <a href="#">Pants</a>
+      <a href="#">Gear</a>
+    </div>
+    <div class="main-nav">
+      <img id="shirts" src="../assets/Shirts.jpeg" alt="">
+      <span>New Arrivals</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+
+<style lang="css">
+
+.landing{
+  display: flex;
+  flex-direction: row;
+  margin-left: 5vw;
+  margin-right: 5vw;
+}
+
+.side-nav{
+  border: solid black 2px;
+  width: 25%;
+  margin-right: 5%;
+  display: flex;
+  flex-direction: column;
+}
+
+a{
+  margin-bottom: 1vw;
+  font-size: 24px;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+.main-nav{
+  border: solid black 2px;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+}
+
+#shirts{
+  width: 60vw;
+}
+
+</style>
